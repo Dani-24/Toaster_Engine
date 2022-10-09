bf000dd45b87512467ec97f57e50ff952bef8209
Dani-24 <79161140+Dani-24@users.noreply.github.com>
2022-10-09 11:25:10 +0200

    MathGeoLib testing

--- ToasterEngine-Code/ModuleSceneIntro.cpp
+++ ToasterEngine-Code/ModuleSceneIntro.cpp
@@ -3,6 +3,8 @@
 #include "ModuleSceneIntro.h"
 #include "Primitive.h"
 
+#include "MathGeoLib/include/MathGeoLib.h"
+
 ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
 }
@@ -19,14 +21,15 @@ bool ModuleSceneIntro::Start()
 	return ret;
 }
 
+update_status ModuleSceneIntro::Update(float dt)
+{
+
+	return UPDATE_CONTINUE;
+}
+
 bool ModuleSceneIntro::CleanUp()
 {
 	LOG("Unloading Intro scene");
 
 	return true;
-}
-
-update_status ModuleSceneIntro::Update(float dt)
-{
-	return UPDATE_CONTINUE;
 }
