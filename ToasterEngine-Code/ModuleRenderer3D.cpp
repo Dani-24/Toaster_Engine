00a729ede9d98bbc92e8524375a676bdfec415f6
Dani-24 <79161140+Dani-24@users.noreply.github.com>
2022-10-24 09:12:17 +0200

    Disable multiviewports flag

--- ToasterEngine-Code/ModuleRenderer3D.cpp
+++ ToasterEngine-Code/ModuleRenderer3D.cpp
@@ -139,7 +139,7 @@ bool ModuleRenderer3D::Init()
 	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;		// Enable Keyboard Controls
 	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;			// Enable Docking
-	io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
+	//io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
 
 	ImGui::StyleColorsDark();
 
