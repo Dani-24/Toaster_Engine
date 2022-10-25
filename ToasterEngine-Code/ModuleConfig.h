7994005c4e4bc8e88f3b9a651a2c33f61a58e296
Dani-24 <79161140+Dani-24@users.noreply.github.com>
2022-10-25 12:13:36 +0200

    why

--- ToasterEngine-Code/ModuleConfig.h
+++ ToasterEngine-Code/ModuleConfig.h
@@ -1,24 +1,22 @@
-#pragma once
-
-#include "Module.h"
-#include "Globals.h"
-
-class ModuleConfig : public Module
-{
-public:
-	ModuleConfig(Application* app, bool start_enabled = true);
-	~ModuleConfig();
-
-	bool Start();
-
-	update_status PreUpdate(float dt);
-	update_status Update(float dt);
-	update_status PostUpdate(float dt);
-
-	bool CleanUp();
-
-private:
-
-};
-
-
+//#pragma once
+//
+//#include "Module.h"
+//#include "Globals.h"
+//
+//class ModuleConfig : public Module
+//{
+//public:
+//	ModuleConfig(Application* app, bool start_enabled = true);
+//	~ModuleConfig();
+//
+//	bool Start();
+//
+//	update_status PreUpdate(float dt);
+//	update_status Update(float dt);
+//	update_status PostUpdate(float dt);
+//
+//	bool CleanUp();
+//
+//private:
+//
+//};
